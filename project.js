a5ac8765c0b3bf54131afbf3a848ed249c0c27c4
Maya <maya.swansbrough@gmail.com>
2021-11-30 17:29:55 -0500

    Saved file names, added pseudocode

---
+++ project.js
@@ -0,0 +1,8 @@
+// Create an integer variable for homeTemp
+// Create a string variable for homeCity
+// Create a list of exotic locales for exoticCity
+// Match exoticCity with a integer variable of exoticTemp
+// Ask the user "Where do you live?" and put answer for homeCity
+// Create a function where homeCity pulls homeTemp from API
+// Tell user "In " homeCity " it is currently " homeTemp
+// Tell user "In " exoticCity " it is currently " exoticTemp
