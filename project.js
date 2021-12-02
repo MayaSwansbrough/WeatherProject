ab318ccff913a3653ba9054145e19c1e9bef4a83
Maya <maya.swansbrough@gmail.com>
2021-12-02 10:36:08 -0500

    Replaced onclick with event handler, added divs

--- project.js
+++ project.js
@@ -1,11 +1,15 @@
-// Ask the user "What temperature is it?"
-// Create an integer variable for temperature
+// Show an error if text input is not a number
 // Ask user "What temperature is it?" and put answer in temperature
+button.addEventListener("click", temperature);
+
 function temperature(num) {
     const questionVal = Number (document.getElementById('question').value);
     console.log(questionVal);
-} 
+}
 // IF temperature is <78, return hotOptions
+if (questionVal >=78) {
+    
+}
 // IF temperature is <65, return coolOptions
 // ELSE return coldOptions
 // Turn whichever return into options
